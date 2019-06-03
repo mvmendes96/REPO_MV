f7793721d788402d64d529e1fcc6deb65eda70db
mvmendes96 <mvmendes96@gmail.com>
2019-06-03 01:09:08 -0300

    Update keys.js

--- Node/config/keys.js
+++ Node/config/keys.js
@@ -1,5 +1,4 @@
 dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
-'mongodb+srv://mvmendes:Abc957qaz2589!@@cluster0-cqenz.mongodb.net/test?retryWrites=true&w=majority'
 module.exports = {
     mongoURI: dbPassword
 };
